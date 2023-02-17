0919f3c7e8f65be278567231ef304340091507dc
Ivon Dimitrova <ivon_y@abv.bg>
2023-02-17 17:30:29 +0200

    tt

---
+++ 3.ToDoList/todo/client/src/components/TodoListt.js
@@ -0,0 +1,40 @@
+import { ToDo } from "./ToDo"
+import { useEffect, useState } from 'react'
+
+export const TodoList = () => {
+
+const [todos, setTodos] = useState([])
+
+useEffect(() => {
+  fetch('http://localhost:3030/jsonstore/my')
+  .then(res => res.json())
+  .then(data => {
+    setTodos(Object.values(data))
+  })
+}, []);
+
+const changeStatusHandler = (singleTodo) => {
+
+  setTodos(old => old.map(x => x._id === singleTodo._id ? {...x, isDone: !singleTodo.isDone} : x ))
+
+}
+
+
+
+  return(
+    <table className="table">
+          <thead>
+            <tr>
+              <th className="table-header-task">Task</th>
+              <th className="table-header-status">Status</th>
+              <th className="table-header-action">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+
+           {todos.map(x => <ToDo changeStatus={changeStatusHandler} key={x._id} {...x} />)} 
+
+          </tbody>
+        </table>
+  )
+}
