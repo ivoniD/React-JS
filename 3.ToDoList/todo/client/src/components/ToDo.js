dba9d562b19b24592f1e74be257a05a89595b305
Ivon Dimitrova <ivon_y@abv.bg>
2023-02-17 17:26:44 +0200

    change title to 'Incomplete'

--- 3.ToDoList/todo/client/src/components/ToDo.js
+++ 3.ToDoList/todo/client/src/components/ToDo.js
@@ -9,10 +9,11 @@ export const ToDo = (props) => {
   return(
     <tr className={className}>
     <td>{props.todo}</td>
-    <td>{props.isDone ? 'Complete' : 'Uncomplete'}</td>
+    <td>{props.isDone ? 'Complete' : 'Incomplete'}</td>
     <td className="todo-action">
       <button onClick={() => props.changeStatus(props)} className="btn todo-btn">Change status</button>
     </td>
   </tr>
   )
 }
+
