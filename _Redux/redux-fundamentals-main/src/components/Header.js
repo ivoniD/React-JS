0c09bec4a736539e6dc3e008a75472c10c53004c
Ivon Dimitrova <ivon_y@abv.bg>
2023-06-21 17:02:31 +0300

    dinamic change - isFavourtie count

--- _Redux/redux-fundamentals-main/src/components/Header.js
+++ _Redux/redux-fundamentals-main/src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+  const favItems = useSelector((state) => {
+    return state.products.products.reduce((accumulator, item) => {
+      return item.isFavourite ? accumulator + 1 : accumulator
+    }, 0)
+  })
+
   return (
     <div className="navbar navbar-dark bg-primary">
       <div className="navbar-header pull-left">
@@ -14,7 +21,7 @@ const Header = () => {
           Products
         </Link>
         <Link to="/favorites" className="btn btn-default text-white navbar-btn">
-          Favorites (0)
+          Favorites ({favItems})
         </Link>
         <Link to="/cart" className="btn btn-default text-white navbar-btn">
           Cart (0)
--- _Redux/redux-fundamentals-main/src/store/index.js
+++ _Redux/redux-fundamentals-main/src/store/index.js
@@ -1,21 +1,15 @@
-import { legacy_createStore as createStore, compose, applyMiddleware} from 'redux'
+import { legacy_createStore as createStore} from 'redux'
 import { combineReducers } from 'redux'
 import { composeWithDevTools } from '@redux-devtools/extension'
 import productsReducer from './products'
-import { addloggingOnDispatch } from '../add-ons/enharcers'
-import { loggerMiddleware, blockActionMiddleware } from '../add-ons/middleware'
 
 const rootReducer = combineReducers({
   products: productsReducer
 })
-const composeEnhancer = compose(addloggingOnDispatch)
-const middlewareEnhancer = applyMiddleware(
-  loggerMiddleware,
-  blockActionMiddleware
-  )
+
 //create store
 const store = createStore(
   rootReducer, 
-  composeWithDevTools(composeEnhancer, middlewareEnhancer)
+  composeWithDevTools()
   );
 export default store 
